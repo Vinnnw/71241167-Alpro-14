8fac55be4b58c575e6e108f50f5a2099639ad701
Arvin <arvin.arvin@ti.ukdw.ac.id>
2025-06-01 22:33:27 +0700

    Create soal2.py

---
+++ soal2.py
@@ -0,0 +1,19 @@
+data_list = [1, 2, 2, 3, 4, 4]
+print("List awal:", data_list)
+data_set_dari_list = set(data_list)
+print("Set dari List:", data_set_dari_list)
+
+data_set = {5, 6, 7, 7, 8}
+print("\nSet awal:", data_set)
+data_list_dari_set = list(data_set)
+print("List dari Set:", data_list_dari_set)
+
+data_tuple = (9, 10, 10, 11)
+print("\nTuple awal:", data_tuple)
+data_set_dari_tuple = set(data_tuple)
+print("Set dari Tuple:", data_set_dari_tuple)
+
+data_set2 = {12, 13, 14}
+print("\nSet awal:", data_set2)
+data_tuple_dari_set = tuple(data_set2)
+print("Tuple dari Set:", data_tuple_dari_set)
